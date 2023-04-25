b879600ff8e17392a18ff285f0a440931539c926
DuttiPOP <126721436+DuttiPOP@users.noreply.github.com>
2023-04-25 20:09:21 +0500

    Реализована запись и чтение данных из таблицы

--- app/src/main/java/com/fitnessapp/tools/DatabaseHelper.java
+++ app/src/main/java/com/fitnessapp/tools/DatabaseHelper.java
@@ -1,20 +1,31 @@
 package com.fitnessapp.tools;
 
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
 import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import com.fitnessapp.user.User;
+
+import java.lang.annotation.Target;
 
 public class DatabaseHelper extends SQLiteOpenHelper{
     private static final String DATABASE_NAME = "GymBuddy.db";
     private static final int DATABASE_VERSION = 1;
     private static final String TABLE_USERS = "users";
     private static final String COLUMN_ID = "id";
+    private static final String COLUMN_FIRSTNAME = "firstName";
+    private static final String COLUMN_LASTNAME = "lastName";
     private static final String COLUMN_EMAIL = "email";
-    private static final String COLUMN_USERNAME = "username";
-    private static final String COLUMN_PASSWORD = "password";
     private static final String COLUMN_AGE = "age";
+    private static final String COLUMN_GENDER = "gender";
     private static final String COLUMN_WEIGHT = "weight";
     private static final String COLUMN_HEIGHT = "height";
+
+
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -24,10 +35,11 @@ public class DatabaseHelper extends SQLiteOpenHelper{
     public void onCreate(SQLiteDatabase db) {
         String CREATE_USERS_TABLE = "CREATE TABLE " + TABLE_USERS + "("
                 + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
-                + COLUMN_USERNAME + " TEXT,"
-                + COLUMN_PASSWORD + " TEXT,"
+                + COLUMN_FIRSTNAME + " TEXT,"
+                + COLUMN_LASTNAME + " TEXT,"
                 + COLUMN_EMAIL + " TEXT,"
                 + COLUMN_AGE + " INTEGER,"
+                + COLUMN_GENDER + " TEXT,"
                 + COLUMN_WEIGHT + " REAL,"
                 + COLUMN_HEIGHT + " REAL"
                 + ")";
@@ -39,4 +51,39 @@ public class DatabaseHelper extends SQLiteOpenHelper{
         db.execSQL("DROP TABLE IF EXISTS "+TABLE_USERS);
         onCreate(db);
     }
+
+    public int saveUser(User user){
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_FIRSTNAME, user.getFirstName());
+        values.put(COLUMN_LASTNAME, user.getLastName());
+        values.put(COLUMN_EMAIL, user.getEmail());
+        values.put(COLUMN_AGE, user.getAge());
+        values.put(COLUMN_FIRSTNAME, user.getGender());
+        values.put(COLUMN_WEIGHT, user.getWeight());
+        values.put(COLUMN_HEIGHT, user.getHeight());
+        return (int) this.getWritableDatabase().insert(TABLE_USERS, null, values);
+
+    }
+
+    public User getUser(int id){
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery("select * from " + TABLE_USERS + " where " + COLUMN_ID + "="+ id, null);
+        User user = new User();
+        if (cursor.moveToFirst()) {
+            user.setFirstName(cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_FIRSTNAME)));
+            user.setLastName(cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_LASTNAME)));
+            user.setEmail(cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_EMAIL)));
+            user.setAge(cursor.getInt(cursor.getColumnIndexOrThrow(COLUMN_AGE)));
+            user.setGender(cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_GENDER)));
+            user.setWeight(cursor.getDouble(cursor.getColumnIndexOrThrow(COLUMN_WEIGHT)));
+            user.setHeight(cursor.getDouble(cursor.getColumnIndexOrThrow(COLUMN_HEIGHT)));
+            db.close();
+            cursor.close();
+        }
+        return user;
+    }
+
+
+
+
 }
