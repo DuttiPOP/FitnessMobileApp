c37a8db511c1037382c2ee7ded1ae76676251dc5
DuttiPOP <126721436+DuttiPOP@users.noreply.github.com>
2023-05-04 13:51:53 +0500

    Обновление календаря

--- app/src/main/java/com/fitnessapp/ProfileActivity.java
+++ app/src/main/java/com/fitnessapp/ProfileActivity.java
@@ -12,19 +12,21 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CalendarView;
 import android.widget.TextView;
 
 import com.fitnessapp.tools.DatabaseHelper;
+import com.fitnessapp.tools.RedirectController;
 import com.fitnessapp.tools.SharedPreferencesHelper;
 import com.fitnessapp.user.User;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 
 public class ProfileActivity extends AppCompatActivity {
 
     Context context = this;
-
     TextView tvUsername;
     Button toWorkoutPlanButton, toWorkoutLogButton, logoutButton, toPersonalDataButton;
-
+    MaterialCalendarView calendarView;
     DatabaseHelper databaseHelper;
     SQLiteDatabase db;
     Cursor cursor;
@@ -35,8 +37,6 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        databaseHelper = new DatabaseHelper(this);
-        db = databaseHelper.getReadableDatabase();
 
         arguments = getIntent().getExtras();
         user = arguments.getParcelable(User.class.getSimpleName());
@@ -47,26 +47,28 @@ public class ProfileActivity extends AppCompatActivity {
         toWorkoutPlanButton = findViewById(R.id.toWorkoutPlanButton);
         toWorkoutLogButton = findViewById(R.id.toWorkoutLogButton);
         logoutButton = findViewById(R.id.logoutButton);
+        calendarView = findViewById(R.id.workoutCalendar);
 
         tvUsername.setText(String.format("Привет, %s!", user.getFirstName()));
 
         toWorkoutPlanButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                RedirectController.switchToAnotherActivity(context, WorkoutPlanActivity.class);
             }
         });
 
         toWorkoutLogButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                RedirectController.switchToAnotherActivity(context, WorkoutLogActivity.class);
             }
         });
 
         toPersonalDataButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(context, PersonalDataActivity.class);
-                startActivity(intent);
+                RedirectController.switchToAnotherActivity(context, PersonalDataActivity.class, user);
             }
         });
 
